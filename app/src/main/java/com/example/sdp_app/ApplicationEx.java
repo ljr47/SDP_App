24dbdcd06a2891a50afc067e35634ded66b77791
ljr47 <ljjr82@gmail.com>
2021-11-28 20:40:22 -0500

    luke-work work (on laptop)

--- app/src/main/java/com/example/sdp_app/ApplicationEx.java
+++ app/src/main/java/com/example/sdp_app/ApplicationEx.java
@@ -6,6 +6,30 @@ import android.os.Handler;
 public class ApplicationEx extends Application{
     private final int STATUS_CHECK_INTERVAL = 500;
     private MyBtEngine mBtEngine;
+
+    //region $BT Device Name
+    private String btDeviceName;
+
+    public String getBtDeviceName() {
+        return btDeviceName;
+    }
+    public void setBtDeviceName(String deviceName) {
+        this.btDeviceName = deviceName;
+    }
+    //endregion
+
+    //region $ BT Device MAC Address
+    private String btDeviceMACAddress;
+
+    public String getBtDeviceMACAddress() {
+        return btDeviceMACAddress;
+    }
+
+    public void setBtDeviceMACAddress(String macAddress) {
+        this.btDeviceMACAddress = macAddress;
+    }
+    //endregion
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -21,7 +45,8 @@ public class ApplicationEx extends Application{
     private final Handler handlerStatusCheck = new Handler();
     private void onBtStatusCheckTimer(){
         if(mBtEngine.getState() == MyBtEngine.BT_STATE_NONE) {
-            mBtEngine.start();
+            String mac = getBtDeviceMACAddress();
+            mBtEngine.start(mac);
         }
     }
     boolean writeBt(byte [] buffer){ return mBtEngine.writeBt(buffer); }
--- app/src/main/java/com/example/sdp_app/MainActivity.java
+++ app/src/main/java/com/example/sdp_app/MainActivity.java
@@ -2,44 +2,52 @@ package com.example.sdp_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BluetoothDevice selectedDevice;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button) findViewById(R.id.test_btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(((ApplicationEx)getApplication()).writeBt("Hello World\n".getBytes(StandardCharsets.UTF_8))){
-                    Toast.makeText(getApplicationContext(), "OK, sent", Toast.LENGTH_SHORT).show();
-                }
+
+        Button btn = findViewById(R.id.button_send);
+        btn.setOnClickListener(v -> {
+            EditText txt = findViewById(R.id.bluetooth_message);
+            String btMessage = txt.getText().toString() + "\n";
+            if(((ApplicationEx)getApplication()).writeBt(btMessage.getBytes(StandardCharsets.UTF_8))){
+                Toast.makeText(getApplicationContext(), "Message sent.", Toast.LENGTH_SHORT).show();
+                txt.setText("");
             }
         });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        final ApplicationEx globalVar = (ApplicationEx) getApplicationContext();
+
+        TextView deviceName = findViewById(R.id.device_name);
+        TextView deviceMAC = findViewById(R.id.device_mac);
+
+        deviceName.setText(globalVar.getBtDeviceName());
+        deviceMAC.setText(globalVar.getBtDeviceMACAddress());
 
     }
 
     public void openBluetooth(View view) {
         Intent intent = new Intent(MainActivity.this, BluetoothActivity.class);
         startActivity(intent);
-//        startActivityForResult(intent, selectedDevice);
     }
-
-//    public void openBluetooth(View view) {
-//        Intent intent =  new Intent(this, BluetoothActivity.class);
-//        startActivity(intent);
-//    }
 }
